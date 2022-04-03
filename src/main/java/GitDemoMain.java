ec6e2ea698be24f70da84e614b804172e8f83102
kanliquan <kanliquan@163.com>
2022-04-03 13:36:00 +0800

    冲突解决hot-fix

--- src/main/java/GitDemoMain.java
+++ src/main/java/GitDemoMain.java
@@ -1,5 +1,10 @@
 public class GitDemoMain {
     public static void main(String[] args) {
-
+        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("hello git5!");
     }
 }
